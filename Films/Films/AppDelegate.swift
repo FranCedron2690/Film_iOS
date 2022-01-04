8b17c7e070aca9f88c5cc84ceb1e53c9b070ad0e
Francisco Jesús Cedrón <fjcedron@hiberus.com>
2022-01-04 10:00:57 +0100

    Copia antes de integrar Crashlytics y Sentra por separado en ramas, para probar su funcionamiento

--- Films/Films/AppDelegate.swift
+++ Films/Films/AppDelegate.swift
@@ -6,11 +6,16 @@
 //
 
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Use the Firebase library to configure APIs.
+        FirebaseApp.configure()
+        
         return true
     }
 
